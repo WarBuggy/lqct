e8b1eb3880824a661b3e9edad004aa3ea6b3ad98
Ho Van Buu <hovanbuu@gmail.com>
2021-08-23 22:30:05 +0700

    add fs-extra`

--- db/config.js
+++ db/config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    host: '',
-    user: '',
-    password: '',
-    initDB: '',
-    port: 0,
+    host: 'localhost',
+    user: 'u1g8f9Dg853',
+    password: '8Q&^r!%v8yTt383e1nFi!&DVN',
+    initDB: 'baosotrung_data',
+    port: 3306,
     logParams: false,
     logLogInfo: false,
 };
+
