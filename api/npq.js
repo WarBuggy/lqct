1b49fa0d79be84f2ad6fef597cb7d0633155fb37
hovanbuu <hovanbuu@gmail.com>
2021-08-23 21:10:37 +0700

    _ simplify error handling

--- api/npq.js
+++ api/npq.js
@@ -4,25 +4,17 @@ let DB = require('../db/db.js');
 module.exports = function (app) {
     app.post('/data/save', async function (request, response) {
         let requestIp = Common.getReadableIP(request);
-        try {
-            let saveMatchResult = await saveMatch(request, requestIp);
-            console.log(saveMatchResult);
-            if (saveMatchResult.success == false) {
-                response.json(saveMatchResult);
-                return;
-            }
-            response.json({
-                success: true,
-                result: 0,
-                id: saveMatchResult.id,
-            });
-        } catch (error) {
-            Common.consoleLog(`(${requestIp}) Unexpected error when ${purpose}: ${error}.`);
-            response.json({
-                success: false,
-                result: 800,
-            });
+        let saveMatchResult = await saveMatch(request, requestIp);
+        console.log(saveMatchResult);
+        if (saveMatchResult.success == false) {
+            response.json(saveMatchResult);
+            return;
         }
+        response.json({
+            success: true,
+            result: 0,
+            id: saveMatchResult.id,
+        });
     });
 
     async function saveMatch(request, requestIp) {
@@ -41,19 +33,27 @@ module.exports = function (app) {
             source: '`lqct_data`.`SAVE_MATCH_DATA`',
             userIP: requestIp,
         };
-        let result = await DB.query(params, logInfo);
-        if (result.resultCode != 0) {
-            let errorCode = result.resultCode;
-            common.consoleLogError('Database error when ' + purpose + '. Error code ' + errorCode + '.');
+        try {
+            let result = await DB.query(params, logInfo);
+            if (result.resultCode != 0) {
+                let errorCode = result.resultCode;
+                common.consoleLogError('Database error when ' + purpose + '. Error code ' + errorCode + '.');
+                return {
+                    success: false,
+                    result: errorCode,
+                };
+            }
+            Common.consoleLog('(' + requestIp + ') Request for ' + purpose + ' was successfully handled.');
             return {
-                success: false,
-                result: errorCode,
+                success: true,
+                id: result.sqlResults[1][0].lastId,
             };
+        } catch (error) {
+            Common.consoleLog(`(${requestIp}) Unexpected error when ${purpose}: ${error}.`);
+            response.json({
+                success: false,
+                result: 800,
+            });
         }
-        Common.consoleLog('(' + requestIp + ') Request for ' + purpose + ' was successfully handled.');
-        return {
-            success: true,
-            id: result.sqlResults[1][0].lastId,
-        };
     };
 };
