34ea9fe5f4b583baaa3fdec2d1a887f976b4015a
hovanbuu <hovanbuu@gmail.com>
2021-08-23 20:56:05 +0700

    _ try catch finally

--- api/npq.js
+++ api/npq.js
@@ -33,26 +33,30 @@ module.exports = function (app) {
             source: '`lqct_data`.`SAVE_MATCH_DATA`',
             userIP: requestIp,
         };
+        let success = true;
+        let id = null;
+        let result = null;
         try {
             let result = await DB.query(params, logInfo);
             if (result.resultCode != 0) {
                 let errorCode = result.resultCode;
                 common.consoleLogError('Database error when ' + purpose + '. Error code ' + errorCode + '.');
-                return {
-                    success: false,
-                    result: errorCode,
-                };
+                success = false;
+                result = errorCode;
+            } else {
+                Common.consoleLog('(' + requestIp + ') Request for ' + purpose + ' was successfully handled.');
+                result = 0;
+                id = result.sqlResults[1][0].lastId;
             }
-            Common.consoleLog('(' + requestIp + ') Request for ' + purpose + ' was successfully handled.');
-            return {
-                success: true,
-                id: result.sqlResults[1][0].lastId,
-            };
         } catch (error) {
             Common.consoleLog(`(${requestIp}) Unexpected error when ${purpose}: ${error}.`);
+            success = false;
+            result = 800;
+        } finally {
             return {
-                success: false,
-                result: 800,
+                success,
+                result,
+                id,
             };
         }
     };
